c179f84dd1f8507f87956d8656716539d572f2c4
nick fan <nickfan81@gmail.com>
2015-09-23 17:59:27 +0800

    init project

---
+++ src/Example.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Nickfan\LaraBloom;
+
+class Example
+{
+
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Nickfan\LaraBloom\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+
+}
